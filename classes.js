3e608bb80e2f21c56b0f7ddb6fbff42eecd58a61
tosca-wilson <toscawilson@Toscas-MacBook-Pro.local>
2019-09-07 21:42:10 +1200

    add js

--- classes.js
+++ classes.js
@@ -5,9 +5,14 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+   one()
     
   // Your turn! Create a new function called `two`, then call it from here.
+  two()
+
+  three()
+
+  makeVisible()
 }
 
 function one () {
@@ -19,7 +24,22 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two')
+
+  two.classList.add('green')
+}
 
 // CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+
+  three.classList.add('plum')
+}
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+
+  makeVisible.classList.add('visible')
+}
